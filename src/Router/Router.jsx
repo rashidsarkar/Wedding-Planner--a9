9d56e21ed0693861d39d84d9685abcd11c2e40e2
rashidsarkar <rashidrock558@gmail.com>
2023-10-08 03:16:59 +0600

    Implement user authentication in Login component

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,14 +1,12 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../MainLayout/MainLayout";
+import NotFound from "../Pages/NotFound";
 import Home from "../Pages/Home";
+import PrivateRoute from "../PrivateRoute/PrivateRoute";
 import About from "../Components/About";
-import Gallery from "../Components/Venues";
-import Card from "../Components/Card";
-import ServiceDetails from "../Pages/services/ServicesDetails";
+import ServicesDetails from "../Pages/services/ServicesDetails";
 import Login from "../Pages/Account/Login";
 import SignUp from "../Pages/Account/SignUp";
-import PrivateRoute from "../PrivateRoute/PrivateRoute";
-import NotFound from "../Pages/NotFound ";
 import Venues from "../Components/Venues";
 
 const router = createBrowserRouter([
@@ -41,7 +39,7 @@ const router = createBrowserRouter([
         path: "/servicesDetails/:idx",
         element: (
           <PrivateRoute>
-            <ServiceDetails></ServiceDetails>,
+            <ServicesDetails></ServicesDetails>,
           </PrivateRoute>
         ),
       },
